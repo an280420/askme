d34765ac1e6b3a336f07ad936f585d46a493f42d
@an280420 <angit280420@gmail.com>
2020-12-15 12:01:59 +0500

    Added helper _question.html.erb. Added question in show model User. Added styles css in assets. Added alt image in assets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
 
   def show
-    @hello = "Привет первый пользователь приложения Askme "
+    @user = User.new(
+      name: 'Vadim',
+      username: 'installero',
+      avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
+    )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('15.12.2020')),
+      Question.new(text: 'Как выучить рельсы?', created_at: Date.parse('15.12.2020'))
+    ]
   end
 end
