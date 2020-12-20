d493d216380f4cfe5832196240d38da66fa574b3
@an280420 <angit280420@gmail.com>
2020-12-20 18:51:36 +0500

    added routes for authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :questions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'show' => 'users#show'
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
