2e05e4aecc882b154583ca08fecab7e9a6355142
@an280420 <angit280420@gmail.com>
2020-12-28 23:31:44 +0500

    edited view and controller User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,8 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find params[:id]
+
     @questions = @user.questions.order(created_at: :desc)
 
     @new_question = @user.questions.build
