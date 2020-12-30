d0c0289c0aa3a570d8cb2ee6d072fb4de3b7b900
@an280420 <angit280420@gmail.com>
2020-12-30 21:40:45 +0500

    combining user creation and session opening

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url, notice: "Пользователь успешно зарегистрирован"
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Пользователь успешно зарегестрирован. Вы вошли в систему как @#{@user.username}."
     else
       render 'new'
     end
